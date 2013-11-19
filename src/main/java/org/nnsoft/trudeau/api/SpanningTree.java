c157fb1425496c0b5ec38eb2f525fadf2a2aa9c9
Simo Tripodi <stripodi@adobe.com>
2013-11-19 15:57:38 +0100

    initial codebase checkin

---
+++ src/main/java/org/nnsoft/trudeau/api/SpanningTree.java
@@ -0,0 +1,31 @@
+package org.nnsoft.trudeau.api;
+
+/*
+ *   Copyright 2013 The Trudeau Project
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+/**
+ * A {@link SpanningTree} of a connected, undirected graph is a tree
+ * composed of all the vertices and some (or perhaps all) of the edges of the Graph.
+ *
+ * @param <V> the Graph vertices type
+ * @param <WE> the Graph weighted edges type
+ * @param <W> the weight type
+ */
+public interface SpanningTree<V, WE, W>
+    extends UndirectedGraph<V, WE>, Weighted<W>
+{
+
+}
---
+++ src/main/java/org/nnsoft/trudeau/api/WeightedPath.java
@@ -0,0 +1,30 @@
+package org.nnsoft.trudeau.api;
+
+/*
+ *   Copyright 2013 The Trudeau Project
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+/**
+ * A {@code Path} whose edges are {@link Weighted}.
+ *
+ * @param <V> the Graph vertices type
+ * @param <E> the Graph weighted edges type
+ * @param <W> the weight type
+ */
+public interface WeightedPath<V, E, W>
+    extends Path<V, E>, Weighted<W>
+{
+
+}
