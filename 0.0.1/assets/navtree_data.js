7bec7e2451b97dd77a5ce2bced0ab62f4bf794fe
Simo Tripodi <stripodi@adobe.com>
2013-12-17 16:03:04 +0100

    Wagon: Deploying apidocs to repository

---
+++ 0.0.1/assets/navtree_data.js
@@ -0,0 +1,9 @@
+var NAVTREE_DATA =
+[ [ "org.nnsoft.trudeau.api", "org/nnsoft/trudeau/api/package-summary.html", [ [ "Description", "org/nnsoft/trudeau/api/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "DirectedGraph", "org/nnsoft/trudeau/api/DirectedGraph.html", null, "" ], [ "Graph", "org/nnsoft/trudeau/api/Graph.html", null, "" ], [ "Mapper", "org/nnsoft/trudeau/api/Mapper.html", null, "" ], [ "MutableGraph", "org/nnsoft/trudeau/api/MutableGraph.html", null, "" ], [ "Path", "org/nnsoft/trudeau/api/Path.html", null, "" ], [ "SpanningTree", "org/nnsoft/trudeau/api/SpanningTree.html", null, "" ], [ "UndirectedGraph", "org/nnsoft/trudeau/api/UndirectedGraph.html", null, "" ], [ "Weighted", "org/nnsoft/trudeau/api/Weighted.html", null, "" ], [ "WeightedPath", "org/nnsoft/trudeau/api/WeightedPath.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "Graphs", "org/nnsoft/trudeau/api/Graphs.html", null, "" ], [ "RevertedGraph", "org/nnsoft/trudeau/api/RevertedGraph.html", null, "" ], [ "VertexPair", "org/nnsoft/trudeau/api/VertexPair.html", null, "" ] ]
+, "" ], [ "Exceptions", null, [ [ "GraphException", "org/nnsoft/trudeau/api/GraphException.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ 0.0.1/reference/lists.js
@@ -0,0 +1,17 @@
+var DATA = [
+      { id:0, label:"org.nnsoft.trudeau.api", link:"org/nnsoft/trudeau/api/package-summary.html", type:"package" },
+      { id:1, label:"org.nnsoft.trudeau.api.DirectedGraph", link:"org/nnsoft/trudeau/api/DirectedGraph.html", type:"class" },
+      { id:2, label:"org.nnsoft.trudeau.api.Graph", link:"org/nnsoft/trudeau/api/Graph.html", type:"class" },
+      { id:3, label:"org.nnsoft.trudeau.api.GraphException", link:"org/nnsoft/trudeau/api/GraphException.html", type:"class" },
+      { id:4, label:"org.nnsoft.trudeau.api.Graphs", link:"org/nnsoft/trudeau/api/Graphs.html", type:"class" },
+      { id:5, label:"org.nnsoft.trudeau.api.Mapper", link:"org/nnsoft/trudeau/api/Mapper.html", type:"class" },
+      { id:6, label:"org.nnsoft.trudeau.api.MutableGraph", link:"org/nnsoft/trudeau/api/MutableGraph.html", type:"class" },
+      { id:7, label:"org.nnsoft.trudeau.api.Path", link:"org/nnsoft/trudeau/api/Path.html", type:"class" },
+      { id:8, label:"org.nnsoft.trudeau.api.RevertedGraph", link:"org/nnsoft/trudeau/api/RevertedGraph.html", type:"class" },
+      { id:9, label:"org.nnsoft.trudeau.api.SpanningTree", link:"org/nnsoft/trudeau/api/SpanningTree.html", type:"class" },
+      { id:10, label:"org.nnsoft.trudeau.api.UndirectedGraph", link:"org/nnsoft/trudeau/api/UndirectedGraph.html", type:"class" },
+      { id:11, label:"org.nnsoft.trudeau.api.VertexPair", link:"org/nnsoft/trudeau/api/VertexPair.html", type:"class" },
+      { id:12, label:"org.nnsoft.trudeau.api.Weighted", link:"org/nnsoft/trudeau/api/Weighted.html", type:"class" },
+      { id:13, label:"org.nnsoft.trudeau.api.WeightedPath", link:"org/nnsoft/trudeau/api/WeightedPath.html", type:"class" }
+
+    ];
